3f1b68f789d84e8b579f3a738566bcc6882555a4
pankake <daniele_commodaro@live.it>
2024-04-17 11:07:01 +0200

    gradle config updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,21 +3,24 @@ plugins {
 }
 
 allprojects {
+    apply<JavaPlugin>()
+
+    java {
+        toolchain {
+            val javaLanguageVersion: Property<JavaLanguageVersion> = project.objects.property(JavaLanguageVersion::class.java)
+            javaLanguageVersion.set(JavaLanguageVersion.of(17))
+            languageVersion.set(javaLanguageVersion)
+        }
+    }
+
     repositories {
         mavenCentral()
-        maven("http://jacamo.sourceforge.net/maven2") {
-            isAllowInsecureProtocol = true
-        }
-        maven("https://jade.tilab.com/maven")
     }
 
     group = "it.unibo.ise"
 }
 
 subprojects {
-
-    apply<JavaPlugin>()
-
     sourceSets {
         main {
             resources {
@@ -27,21 +30,17 @@ subprojects {
     }
 
     dependencies {
-        implementation("org.jason-lang", "jason", "2.3")
+        implementation("net.sf.jason", "jason", "2.3")
     }
-
-    configure<JavaPluginConvention> {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-    }
-
 
     file(projectDir).listFiles().filter { it.extension == "mas2j" }.forEach { mas2jFile ->
-        task<JavaExec>("run${mas2jFile.nameWithoutExtension.capitalize()}Mas") {
+        task<JavaExec>("run${mas2jFile.nameWithoutExtension}") {
             group = "run"
             classpath = sourceSets.getByName("main").runtimeClasspath
-            main = "jason.infra.centralised.RunCentralisedMAS"
+            mainClass.set("jason.infra.centralised.RunCentralisedMAS")
             args(mas2jFile.path)
             standardInput = System.`in`
+            javaLauncher.set(javaToolchains.launcherFor(java.toolchain))
         }
     }
 }
